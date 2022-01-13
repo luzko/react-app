fe764b3193ae51b274f76014814f5e10dd0d3313
Dzmitry Luzko <dluzko@godaddy.com>
2022-01-13 15:53:04 +0300

    Add tests

---
+++ src/component/Footer/Footer.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import renderer from 'react-test-renderer';
+import Footer from "./Footer";
+
+test('snapshot test for footer', () => {
+  const component = renderer.create(
+      <Footer/>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
